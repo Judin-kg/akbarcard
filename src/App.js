35bde807c67a540e915321352a338a9f3e9b284a
Judin kg <judinkg2003@gmail.com>
2025-11-13 13:34:16 +0530

    Initial commit after edits

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Passport from './components/Passport';
+
+
+function App() {
+  return (
+    <div>
+      <Passport />
+      
+    </div>
+  );
+}
+
+export default App;
